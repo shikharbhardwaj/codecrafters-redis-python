ced6fffb74b28ab7456921838fe976d7fa8152f5
shikharbhardwaj <shikhar.bhardwaj@codenation.co.in>
2021-01-24 02:58:18 +0530

    Listen on 6379

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-def main():
-    print("Your code goes here!")
+import socket
 
-    # Uncomment this to pass the first stage
-    #
-    # import socket
-    # s = socket.create_server(("localhost", 6379), reuse_port=True)
-    # s.accept() # wait for client
+from app import utils
+
+
+def main():
+    reuse_port:bool = not utils.is_windows()
+    s = socket.create_server(("localhost", 6379), reuse_port=reuse_port)
+
+    s.accept()  # wait for client
 
 
 if __name__ == "__main__":
